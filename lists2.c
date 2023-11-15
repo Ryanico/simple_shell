92f36dd7a672226b9f4a196ef55ad54574c60a74
Ryanico <ryangisa2001@gmail.com>
2023-11-15 04:10:21 -0800

    all about separators II

---
+++ lists2.c
@@ -0,0 +1,61 @@
+#include "holberton.h"
+
+/**
+ * adds_rvar_node - adds a variable at the end
+ * of a r_vars list.
+ * @Head: head of the linked list.
+ * @lvars: length of the variable.
+ * @vals: value of the variable.
+ * @lvals: length of the value.
+ * Return: address of the head.
+ */
+r_vars *adds_rvar_node(r_vars **Head, int lvars, char *vals, int lvals)
+{
+	r_vars *New, *Temp;
+
+	new = malloc(sizeof(r_vars));
+	if (New == NULL)
+		return (NULL);
+
+	New->len_var = lvars;
+	New->vals = vals;
+	New->len_val = lvals;
+
+	New->next = NULL;
+	Temp = *head;
+
+	if (Temp == NULL)
+	{
+		*Head = New;
+	}
+	else
+	{
+		while (Temp->next != NULL)
+			Temp = Temp->next;
+		Temp->next = New;
+	}
+
+	return (*Head);
+}
+
+/**
+ * frees_rvar_list - frees a r_vars list
+ * @Head: head of the linked list.
+ * Return: no return.
+ */
+void frees_rvar_list(r_vars **Head)
+{
+	r_vars *Temp;
+	r_vars *currs;
+
+	if (Head != NULL)
+	{
+		currs = *Head;
+		while ((Temp = currs) != NULL)
+		{
+			currs = currs->next;
+			free(Temp);
+		}
+		*Head = NULL;
+	}
+}
